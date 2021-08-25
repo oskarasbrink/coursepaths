bc16e10a4e7883b7b5c88083fe1c3195bc0e891e
Ammar <ammar@student-211-99.eduroam.uu.se>
2021-08-25 14:44:35 +0200

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import "./App.css";
 import { ArcherContainer, ArcherElement } from "react-archer";
 import pathways from "./pathwaysContents.json";
-import mix from "./pics/mix.png"
-import aws from "./pics/aws.png"
-import databricks from "./pics/databricks.png"
-import wasp from "./pics/wasp2.png"
-import uu from "./pics/uu2.png"
+import mix from "./pics/mix.png";
+import aws from "./pics/aws.png";
+import databricks from "./pics/databricks.png";
+import wasp from "./pics/wasp2.png";
+import uu from "./pics/uu2.png";
+import lamastex from "./pics/DISP-lamastex-1600x524.jpeg";
 
 function App() {
   return (
-    <div className="w-full p-2 text-gray-100 bg-gray-100 font-mono">
-      <div className="flex justify-center items-center w-full md:p-7 bg-gray-800">
-        <p className=" mt-10 md:text-4xl text-xl p-5 pb-12 text-center">
+    <div className="w-full text-gray-100 bg-gray-100 font-mono">
+      <div className="relative w-full">
+        <img className="w-full" src={lamastex} alt=""></img>
+        <div className="centered w-full md:text-4xl text-xl text-center text-white font-medium subpixel-antialiased shadow-lg brightness-110">
           <a href="https://lamastex.github.io/scalable-data-science/sds/3/x/">
             ScaDaMaLe Course Pathways
           </a>
-        </p>
+        </div>
       </div>
+
       <div className="md:p=24 md:pt-24 p-7">
         <ArcherContainer strokeColor="#9ca3af">
           {Object.keys(pathways.allRows).map((row, index) => (
@@ -68,50 +71,65 @@ function App() {
           ))}
         </ArcherContainer>
       </div>
-      <div className="flex justify-center items-center w-full md:p-7 p-3 bg-gray-800">
+      <div className="flex justify-center items-center w-full md:p-7 p-3 bg-white text-gray-900">
         <div className="flex justify-center items-center w-full space-x-10 ">
           <div className="flex justify-start ">
             <p>2020-2021 instance was Sponsored by</p>
           </div>
           <div className="flex flex-col justify-start items-start w-2/3 space-y-3 ">
-            <div className="flex justify-start items-start space-x-2">
-            <img
-              className="bg-white h-5"
-              src={mix}
-              alt=""
-            />
-              <a className="hover:text-indigo-200 text-sm md:text-md xl:text/lg" href="https://combient.com/mix" target="_blank">Combient Mix</a>
+            <div className="flex justify-start items-center space-x-2">
+              <img className="h-8 w-12 object-scale-down border border-gray-50 p-1 rounded-sm " src={mix} alt="" />
+
+              <a
+                className="hover:text-indigo-200 text-sm md:text-md xl:text/lg"
+                href="https://combient.com/mix"
+                target="_blank"
+              >
+                Combient Mix
+              </a>
             </div>
-            <div className="flex justify-start items-start space-x-2 ">
-            <img
-              className=" h-5"
-              src={wasp}
-              alt=""
-            />
-              <a className="hover:text-indigo-200 text-sm md:text-md xl:text/lg" href="https://wasp-sweden.org/" target="_blank">Wallenberg AI, Autonomous Systems and Software Program</a>
+            <div className="flex justify-start items-center space-x-2 ">
+              <img className="h-8 w-12 object-scale-down border border-gray-50 p-1 rounded-sm " src={wasp} alt="" />
+              <a
+                className="hover:text-indigo-200 text-sm md:text-md xl:text/lg"
+                href="https://wasp-sweden.org/"
+                target="_blank"
+              >
+                Wallenberg AI, Autonomous Systems and Software Program
+              </a>
             </div>
-            <div className="flex justify-start items-start space-x-2 ">
-            <img
-              className="bg-white h-5"
-              src={uu}
-              alt=""
-            />
-              <a className="hover:text-indigo-200 text-sm md:text-md xl:text/lg" href="https://math.uu.se/" target="_blank">Department of Mathematics and Centre for Interdisciplinary Mathematics at Uppsala University</a>
+            <div className="flex justify-start items-center space-x-2 ">
+              <img className="h-8 w-12 object-scale-down border border-gray-50 p-1 rounded-sm " src={uu} alt="" />
+              <a
+                className="hover:text-indigo-200 text-sm md:text-md xl:text/lg"
+                href="https://math.uu.se/"
+                target="_blank"
+              >
+                Department of Mathematics and Centre for Interdisciplinary
+                Mathematics at Uppsala University
+              </a>
             </div>
-            <div className="flex justify-start items-start space-x-2 ">
-            <img
-              className="bg-white h-5"
-              src={databricks}
-              alt=""
-            />
-              <a className="hover:text-indigo-200 text-sm md:text-md xl:text/lg" href="http://databricks.com/" target="_blank">Databricks</a>
-              <div className="text-sm md:text-md xl:text/lg"> University Alliance with</div>
-              <img
-              className="bg-white h-5"
-              src={aws}
-              alt=""
-            />
-              <a className="hover:text-indigo-200 text-sm md:text-md xl:text/lg" href="https://aws.amazon.com/" target="_blank">AWS</a>
+            <div className="flex justify-start items-center space-x-2 ">
+              <img className="h-8 w-12 object-scale-down border border-gray-50 p-1 rounded-sm " src={databricks} alt="" />
+              <a
+                className="hover:text-indigo-200 text-sm md:text-md xl:text/lg"
+                href="http://databricks.com/"
+                target="_blank"
+              >
+                Databricks
+              </a>
+              <div className="text-sm md:text-md xl:text/lg">
+                {" "}
+                University Alliance with
+              </div>
+              <img className="h-8 w-12 object-scale-down border border-gray-50 p-1 rounded-sm " src={aws} alt="" />
+              <a
+                className="hover:text-indigo-200 text-sm md:text-md xl:text/lg"
+                href="https://aws.amazon.com/"
+                target="_blank"
+              >
+                AWS
+              </a>
             </div>
           </div>
         </div>
